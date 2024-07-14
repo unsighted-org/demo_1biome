e64a5979ab125e38604eaed4d96c7940b2470514
Unsighted-org <161677463+Unsighted-org@users.noreply.github.com>
2024-07-14 10:00:35 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
-// server.js
-const { createServer } = require('https');
+// // server.js
+// const { createServer } = require('https');
+// const { parse } = require('url');
+// const next = require('next');
+// const fs = require('fs');
+
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// const httpsOptions = {
+//   key: fs.readFileSync('./certificates/localhost-key.pem'),
+//   cert: fs.readFileSync('./certificates/localhost.pem')
+// };
+
+// app.prepare().then(() => {
+//   createServer(httpsOptions, (req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     handle(req, res, parsedUrl);
+//   }).listen(3000, (err) => {
+//     if (err) throw err;
+//     console.log('> Ready on https://localhost:3000');
+//   });
+// });
+
+const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-const fs = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: fs.readFileSync('./certificates/localhost-key.pem'),
-  cert: fs.readFileSync('./certificates/localhost.pem')
-};
+const PORT = process.env.PORT || 3000;
 
 app.prepare().then(() => {
-  createServer(httpsOptions, (req, res) => {
+  createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, (err) => {
+  }).listen(PORT, (err) => {
     if (err) throw err;
-    console.log('> Ready on https://localhost:3000');
+    console.log(`> Ready on http://localhost:${PORT}`);
   });
 });
